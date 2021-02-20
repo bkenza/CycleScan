4c83c60f0065db2449a3c3f69359b77ecb3c310f
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-02-20 15:21:08 -0800

    fixes

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,12 +1,12 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import HomeScreen from '../screens/HomeScreen';
-import ScannerScreen from '../screens/ScannerScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import HomeScreen from "../screens/HomeScreen";
+import ScannerScreen from "../screens/ScannerScreen";
+import { BottomTabParamList, TabOneParamList, TabTwoParamList } from "../types";
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -16,12 +16,15 @@ export default function BottomTabNavigator() {
   return (
     <BottomTab.Navigator
       initialRouteName="Home"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
       <BottomTab.Screen
         name="Home"
         component={HomeNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-home" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-home" color={color} />
+          ),
         }}
       />
       <BottomTab.Screen
@@ -35,7 +38,10 @@ export default function BottomTabNavigator() {
   );
 }
 
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof Ionicons>["name"];
+  color: string;
+}) {
   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
@@ -47,7 +53,7 @@ function HomeNavigator() {
       <HomeStack.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerTitle: 'Home' }}
+        options={{ headerTitle: "CycleScan" }}
       />
     </HomeStack.Navigator>
   );
@@ -61,7 +67,7 @@ function ScannerNavigator() {
       <ScannerStack.Screen
         name="Scanner"
         component={ScannerScreen}
-        options={{ headerTitle: 'Scanner' }}
+        options={{ headerTitle: "Scanner" }}
       />
     </ScannerStack.Navigator>
   );
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
   },
   textContainer: {
     width: "100%",
-    height: "55%",
+    height: "57%",
     backgroundColor: "#FFFBF4",
     display: "flex",
     flexDirection: "column",
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     width: "100%",
-    height: "45%",
+    height: "43%",
   },
   img: {
     width: "100%",
@@ -98,5 +98,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#000000",
     textAlign: "center",
+    marginBottom: 5,
   },
 });
